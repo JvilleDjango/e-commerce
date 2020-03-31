cda8b39b4aec9950ea91e191c593e8b146d9129d
Jeremy R Miller <jeremymiller110@hotmail.com>
2020-03-31 08:39:35 -0700

    Error Boundry
    Added Error boundry and updates spinner

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
@@ -6,13 +6,16 @@ import { createStructuredSelector } from "reselect";
 import "./App.css";
 
 import Header from "../components/header/header";
-import SignInOut from "./signinout/sign-in-out";
-import HomePage from "./home/HomePage";
-import ShopPage from "./shop/ShopPage";
-import CheckOut from "./checkout/check-out";
+import Spinner from "../components/spinner/spinner";
+import ErrorBoundry from "../components/error-boundry/error-boundry";
 
 import { selectCurrentUser } from "../redux/user/user-selector";
 import { checkUserSession } from "../redux/user/user-actions";
+
+const HomePage = lazy(() => import("./home/HomePage"));
+const ShopPage = lazy(() => import("./shop/ShopPage"));
+const SignInOut = lazy(() => import("./signinout/sign-in-out"));
+const CheckOut = lazy(() => import("./checkout/check-out"));
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
@@ -23,14 +26,18 @@ const App = ({ checkUserSession, currentUser }) => {
     <div>
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route exact path="/checkout" component={CheckOut} />
-        <Route
-          exact
-          path="/signin"
-          render={() => (currentUser ? <Redirect to="/" /> : <SignInOut />)}
-        />
+        <ErrorBoundry>
+          <Suspense fallback={<Spinner />}>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route exact path="/checkout" component={CheckOut} />
+            <Route
+              exact
+              path="/signin"
+              render={() => (currentUser ? <Redirect to="/" /> : <SignInOut />)}
+            />
+          </Suspense>
+        </ErrorBoundry>
       </Switch>
     </div>
   );
